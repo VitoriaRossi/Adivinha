b1f2637ef219596d8b574f3335066cb245977b5c
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-11-04 19:50:23 -0300

    Adicionei a adivinha.

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("O que é, o que é? Todo mês tem, menos abril.\n"); Thread.Sleep(5000);
+Console.WriteLine("A letra 'o'.");
